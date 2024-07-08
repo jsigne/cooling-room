5da055ee71186da9cc282b0f0a3a9398ca1d2662
jsigne <josie.signe@alten.com>
2024-07-08 15:25:30 +0200

    Send cooling alert message to topic

--- src/main/java/com/example/cold_room/processor/CoolingProcessor.java
+++ src/main/java/com/example/cold_room/processor/CoolingProcessor.java
@@ -5,6 +5,7 @@ import com.example.cold_room.model.Cooling;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,9 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class CoolingProcessor {
-
     private static final Serde<String> STRING_SERDE = Serdes.String();
 
     private final CoolingRepository repository;
-
     private final CoolingParser coolingParser;
 
     @Autowired
@@ -31,6 +30,14 @@ public class CoolingProcessor {
             repository.save(cooling);
             return KeyValue.pair(cooling.getIdRoom().toString(), cooling);
         });
-        messageStream.to("alert-topic", Produced.with(STRING_SERDE, STRING_SERDE));
+
+        coolingStream.filter((k, v) -> v.getTemperature() >= -20 || !v.getIsCooling() && v.getTemperature() > -35)
+                .map((k,v) -> KeyValue.pair(k, v.toString()))
+                .to("alert-topic");
+
+        coolingStream.filter((k, v) -> v.getIsCooling() && -35 <= v.getTemperature() && v.getTemperature() <=-21 )
+                .map((k,v) -> KeyValue.pair(k, v.toString()))
+                .to("warning-topic");
     }
+
 }
