feef892e8fb17a0a01205f3efb378ad974e823a8
jsigne <josie.signe@alten.com>
2024-07-09 16:35:47 +0200

    Fix AlertParser test

--- src/test/java/com/example/cold_room/scanalert/AlertParserTest.java
+++ src/test/java/com/example/cold_room/scanalert/AlertParserTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,12 +19,15 @@ class AlertParserTest {
     @Test
     void parse_shouldReturnAlert_whenMatchingString(){
         // Given
+        LocalDateTime date = LocalDateTime.now();
         Alert expectedAlert = new Alert();
         expectedAlert.setIdRoom(3);
         expectedAlert.setTemperature(-17.62);
+        expectedAlert.setAlertDate(date);
+
 
         // When
-        Alert actualAlert = alertParser.parse("Cooling(id=64276, idRoom=3, isCooling=false, temperature=-17.62, consumption=0.0, messageDate=null)");
+        Alert actualAlert = alertParser.parse("Cooling(id=64276, idRoom=3, isCooling=false, temperature=-17.62, consumption=0.0, messageDate=%s)".formatted(date.toString()));
 
         // Then
         assertThat(actualAlert).usingRecursiveComparison().isEqualTo(expectedAlert);
